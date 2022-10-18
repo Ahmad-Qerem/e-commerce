ab55b32db4f4da0ac799328ded088cab9c9e40d4
Ahmad-Qerem <alqerema@gmail.com>
2022-10-18 20:21:37 +0300

    fix HM menu

--- src/components/Header/NaveBar/BurgerMenu/index.js
+++ src/components/Header/NaveBar/BurgerMenu/index.js
@@ -1,23 +1,34 @@
 import styles from "./styles.module.css"
-
+import {useState} from 'react' 
 
 const ListItemsText = ["shop","fabric","journal","about"];
 
 const BurgerMenu = (  )=> {
-    return(
-        <div className={styles.MyBurgerMenu}>
-            <img className={styles.IconBM} src="./images/icon-hamburger.svg"/>
-            <div className={styles.Menu}>
-                {
-                    ListItemsText.map((item)=>{
-                        return(
-                            <div className={styles.MenuItem}> {item} <div className={`${styles.down} ${styles.margin}`}>&#62;</div></div>   
-                        );
-                    })
-                }
-                
+    const [flag, setFlag] = useState(false);
+    function HandelMenuOnClick() {
+      setFlag(!flag);
+    }
+    return (
+      <>
+        <img onClick={HandelMenuOnClick} className={styles.IconBM} src="./images/icon-hamburger.svg" />
+        {flag && <div className={styles.MyBurgerMenu}>
+          <div className={styles.Menu}>
+            <div className={styles.RightSide}>
+                <div className={styles.LogIn}>login</div>
+                  <a href=""><img className={styles.MyImg} src="./images/search.svg" alt="search"/></a>
+                  <a href=""><img className={styles.MyImg} src="./images/heart.svg" alt="favorite"/></a>
+                  <a href=""><img className={styles.MyImg} src="./images/Shape.svg" alt="shop"/></a>
             </div>
-        </div>
+            {ListItemsText.map((item) => {
+              return (
+                <div className={styles.MenuItem}>
+                  {item}<div className={`${styles.down} ${styles.margin}`}>&#62;</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>}
+      </>
     );
 
 };
