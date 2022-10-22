7099b3ae97a2a40dac2da3c48339ae3260af46b7
Ahmad-Qerem <alqerema@gmail.com>
2022-10-22 17:14:09 +0300

    try num 4

--- src/components/Header/NaveBar/RightSide/index.js
+++ src/components/Header/NaveBar/RightSide/index.js
@@ -5,9 +5,9 @@ const RightSide = ( )=>{
     return(
         <div className={styles.RightSide}>
             <div  className={styles.LogIn}>login</div>
-            <a href=""><img className={styles.MyImg} src="./images/search.svg" alt="search"/></a>
-            <a href=""><img className={styles.MyImg} src="./images/heart.svg" alt="favorite"/></a>
-            <a href=""><img className={styles.MyImg} src="./images/Shape.svg" alt="shop"/></a>
+            <a href=""><img className={styles.MyImg} src="/images/search.svg" alt="search"/></a>
+            <a href=""><img className={styles.MyImg} src="/images/heart.svg" alt="favorite"/></a>
+            <a href=""><img className={styles.MyImg} src="/images/Shape.svg" alt="shop"/></a>
         </div>
     );
 };
